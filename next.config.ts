d7a98b3f1bb8d41de084b2d61f480972160b9925
Valeria <waleria.shadrina@gmail.com>
2025-09-01 15:45:31 +0200

    Update next config and ts config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "pokeapi.com" },
+      { hostname: "raw.githubusercontent.com" },
+    ],
+  },
 };
-
 export default nextConfig;
